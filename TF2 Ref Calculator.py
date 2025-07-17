c8e3ec9d4268c484898cadb0d404d94ba906add2
PochaSevruka <pochasevruka@gmail.com>
2025-07-17 17:51:22 +0300

    Ref Calculator for TF2
    This script multiplies the price of an item by its quantity. Speeds up the price calculation process a little bit.

---
+++ TF2 Ref Calculator.py
@@ -0,0 +1,44 @@
+#TF2 Ref Calculator
+# Discord of Author: seryozniman
+
+# ------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------
+while True:
+    ItemPrice = float(input("Enter Item Price: "))
+    quantity = int(input("Enter Quantity: "))
+# ------------------------------------------------------------------
+    integers = int(ItemPrice // 1)
+    decimal = int((round(ItemPrice % 1, 2)) * 100)
+# ------------------------------------------------------------------
+    ProductIntegers = integers * quantity
+    ProductDecimals = decimal
+    ProductDecimals11 = 0
+    k = decimal // 11
+    ProductDecimals05 = decimal - k * 11
+    BoolSummand = True
+# ------------------------------------------------------------------
+    if decimal % 11 == 0:
+        ProductDecimals = decimal * quantity
+    else:
+        for i in range(1, quantity):
+            print("+123")
+            ProductDecimals += (k * 11)
+            if BoolSummand == True:
+                ProductDecimals += ProductDecimals05 + 1
+                BoolSummand = False
+
+            else:
+                ProductDecimals += ProductDecimals05
+                BoolSummand = True
+            print(BoolSummand)
+# ------------------------------------------------------------------
+    if ProductDecimals >= 99:
+        ProductIntegers += ProductDecimals // 99
+        ProductDecimals = ProductDecimals % 99
+# -----------------------------------------------------------------
+    if len(str(ProductDecimals)) == 1:
+      ProductDecimals = "0" + str(ProductDecimals)
+# -----------------------------------------------------------------
+    print(f"Price: {ProductIntegers}.{str(ProductDecimals)}")
